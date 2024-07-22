87c121a56af008ac2f08787ff1acb74ceeb7599a
emrekocamann <emre.kcmn15@gmail.com>
2024-07-22 15:19:14 +0300

    config changed

--- library-service/src/main/java/com/kitaplik/library_service/controller/LibraryController.java
+++ library-service/src/main/java/com/kitaplik/library_service/controller/LibraryController.java
@@ -7,6 +7,7 @@ import com.kitaplik.library_service.service.LibraryService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,9 @@ public class LibraryController {
     Logger logger = LoggerFactory.getLogger(LibraryController.class);
     private final LibraryService libraryService;
     private final Environment environment;
+
+    @Value("${library.service.count}")
+    private Integer count;
 
     public LibraryController(LibraryService libraryService, Environment environment) {
         this.libraryService = libraryService;
@@ -45,5 +49,10 @@ public class LibraryController {
     @GetMapping
     public ResponseEntity<List<String>> getAllLibraries(){
         return ResponseEntity.ok(libraryService.getAllLibraries());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        return ResponseEntity.ok("Library service count: "+count);
     }
 }
